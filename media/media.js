2627540609e90d83fc83ae78f83da2b94edb04a6
masoume <masume.ndri@gmail.com>
2021-12-23 15:44:11 -0800

    media class complete

--- media/media.js
+++ media/media.js
@@ -1,29 +1,34 @@
 class Media{
-    constructor(thumbnail,author,genre,productionYear){
+    constructor(thumbnail,author,genre,date){
         this.author = author
         this.genre = genre
         this.thumbnail = thumbnail
-        this.productionYear = productionYear
+        this.date = date
     }
 }
 class Movie extends Media{
-    constructor(director,superStar){
-        super(thumbnail,author,genre,productionYear);
+    constructor(thumbnail,author,genre,date,director,superStar){
+        super(thumbnail,author,genre,date);
         this.superStar = superStar
         this.director = director
         this.MovieInfo = function(){
-            console.log(`thumbnail: ${this.thumbnail},author:${this.author},genre:${this.genre}, productionYear:${this.productionYear}, superStar: ${this.superStar}, director:${this.director}`)
+            console.log(`{thumbnail: ${this.thumbnail}, author:${this.author}, genre:${this.genre}, production year:${this.date}, superStar: ${this.superStar}, director:${this.director}}`)
         }
     }
 }
 class Book extends Media{
-    constructor(circulation , pages){
-        super(thumbnail,author,genre,productionYear)
-        this.circulation = circulation
+    constructor(thumbnail,author,genre,date,country, pages,language){
+        super(thumbnail,author,genre,date)
+        this.country = country
+        this.language = language
         this.pages = pages
+        this.BookInfo = function(){
+            console.log(`{thumbnail: ${this.thumbnail}, author:${this.author}, genre:${this.genre}, Publication date:${this.date}, country: ${this.country}, pager:${this.pages} , language: ${this.language}}`)
+        }
     }
 }
 
-const interStellar = new Movie("interStellar","Jonathan Nolan","sci-fi",2014,"Matthew McConaughey", "Christopher Nolan")
+const interStellar = new Movie("inter Stellar","Jonathan Nolan","sci-fi",2014,"Matthew McConaughey", "Christopher Nolan")
 interStellar.MovieInfo()
-
+const meBeforeYou = new Book("Me Before You" ,"Jojo Moyes","romance novel",2012,"United Kingdom",480,"English")
+meBeforeYou.BookInfo()
