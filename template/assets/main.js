2c8af15ff3a7c34c2e3ef462dd2c338c10f86937
masoume <masume.ndri@gmail.com>
2021-12-23 19:48:45 -0800

    90percent of responsive done

--- template/assets/main.js
+++ template/assets/main.js
@@ -1,12 +1,21 @@
-const burger = document.querySelector(".hamburger");
-const nav = document.querySelector(".nav-links");
-const navLinks = document.querySelectorAll(".nav-links li");
+// const burger = document.querySelector(".hamburger");
+// const nav = document.querySelector(".nav-links");
+// const navLinks = document.querySelectorAll(".nav-links li");
 
-burger.addEventListener("click", () => {
-	nav.classList.toggle("nav-active");
-	navLinks.forEach(
-		(link, index) =>
-			(link.style.animation = link.style.animation ? "" : `navLinkFade 0.5s ease forwards ${index / 5 + 0.3}s`)
-	);
-	burger.classList.toggle("burger-active");
-});
+// burger.addEventListener("click", () => {
+// 	nav.classList.toggle("nav-active");
+// 	navLinks.forEach(
+// 		(link, index) =>
+// 			(link.style.animation = link.style.animation ? "" : `navLinkFade 0.5s ease forwards ${index / 5 + 0.3}s`)
+// 	);
+// 	burger.classList.toggle("burger-active");
+// });
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
